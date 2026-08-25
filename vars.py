84522c8c8ced51fffec6c1e29a17b9a76e1cc782
hariomrathour9046 <hariomrathour9046@gmail.com>
2026-08-25 18:47:30 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,17 +3,17 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "27400172"))
-API_HASH = environ.get("API_HASH", "56d0a75c5f9a9de6beb5452aa63c2d36")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8396814092:AAHtUGEPciF5LdiCo572-TczY3EZwx0Otis")
+API_ID = int(environ.get("API_ID", "39946374"))
+API_HASH = environ.get("API_HASH", "7fdea3171ea895e2f9f15f22ff71207c")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8896836032:AAFhueJ-DxAiY55ya2JV0PM-4jprVQ9ujsc")
 
-OWNER = int(environ.get("OWNER", "7540570087"))
+OWNER = int(environ.get("OWNER", "8417906815"))
 CREDIT = environ.get("CREDIT", "")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '7540570087,7061670859').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '8417906815').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '7540570087,7061670859').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '8417906815').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
